b0eb23e1f9a754a6d822f0fc202c5b9bba14d943
Connor Haskins <ConnorAHaskins@gmail.com>
2019-06-20 21:47:43 -0700

    addt'l 9x9 image kernels

--- src/kernels.h
+++ src/kernels.h
@@ -26,7 +26,7 @@ const float32 kernels3x3[5][9] = {
 	// emboss kernel
 	{-2.0f, -1.0f, 0.0f,
 	-1.0f, 1.0f, 1.0f,
-	0.0f, 1.0f, 2.0f},
+	0.0f, 1.0f, 2.0f}
 };
 
 const uint32 normalKernel3x3Index = 0;
@@ -41,11 +41,7 @@ const uint32 embossKernel3x3Index = 4;
 
 
 // 5x5 Kernels
-constexpr auto blurLowWeight = (1.0f/40.0f);
-constexpr auto blurMidWeight = (2.0f/40.0f);
-constexpr auto blurHighWeight = (4.0f/40.0f);
-
-const float32 kernels5x5[2][25] = {
+const float32 kernels5x5[8][25] = {
 	// normal kernel
 	{0, 0, 0, 0, 0,
 	0, 0, 0, 0, 0,
@@ -53,15 +49,69 @@ const float32 kernels5x5[2][25] = {
 	0, 0, 0, 0, 0,
 	0, 0, 0, 0, 0},
 
+	// sharpen 1 kernel
+	{-1, -1, -1, -1, -1,
+	-1, 1, 1, 1, -1,
+	-1, 1, 9, 1, -1,
+	-1, 1, 1, 1, -1,
+	-1, -1, -1, -1, -1},
+
+	// sharpen 2 kernel
+	{-1, -1, -1, -1, -1,
+	-1, -1, -1, -1, -1,
+	-1, -1, 25, -1, -1,
+	-1, -1, -1, -1, -1,
+	-1, -1, -1, -1, -1},
+
+	// outline kernel
+	{1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+	1.0f, 1.0f, -24.0f, 1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f, 1.0f, 1.0f},
+
+	// emboss kernel
+	{-4.0f, -3.0f, -2.0f -1.0f, 0.0f
+	-3.0f, -2.0f, -1.0f, 0.0f, 1.0f,
+	-2.0f, -1.0f, 1.0f, 1.0f, 2.0f,
+	-1.0f, 0.0f, 1.0f, 2.0f, 3.0f,
+	0.0f, 1.0f, 2.0f, 3.0f, 4.0f},
+
+	// edge kernel 1
+	{(2.0f / 18.0f), (2.0f / 18.0f), (4.0f / 18.0f), (2.0f / 18.0f), (2.0f / 18.0f),
+	(1.0f / 18.0f), (1.0f / 18.0f), (2.0f / 18.0f), (1.0f / 18.0f), (1.0f / 18.0f),
+	0, 0, 0, 0, 0,
+	(-2.0f / 18.0f), (-2.0f / 18.0f), (-4.0f / 18.0f), (-2.0f / 18.0f), (-2.0f / 18.0f),
+	(-1.0f / 18.0f), (-1.0f / 18.0f), (-2.0f / 18.0f), (-1.0f / 18.0f), (-1.0f / 18.0f)},
+
+	// edge kernel 2
+	{(2.0f / 18.0f), (1.0f / 18.0f), 0, (-1.0f / 18.0f), (-2.0f / 18.0f),
+	(2.0f / 18.0f), (1.0f / 18.0f), 0, (-1.0f / 18.0f), (-2.0f / 18.0f),
+	(4.0f / 18.0f), (2.0f / 18.0f), 0, (-2.0f / 18.0f), (-4.0f / 18.0f),
+	(2.0f / 18.0f), (1.0f / 18.0f), 0, (-1.0f / 18.0f), (-2.0f / 18.0f),
+	(2.0f / 18.0f), (1.0f / 18.0f), 0, (-1.0f / 18.0f), (-2.0f / 18.0f)},
+
 	// blur kernel
-	{blurLowWeight, blurLowWeight, blurMidWeight, blurLowWeight, blurLowWeight,
-	blurLowWeight, blurMidWeight, blurMidWeight, blurMidWeight, blurLowWeight,
-	blurMidWeight, blurMidWeight, blurHighWeight, blurMidWeight, blurMidWeight,
-	blurLowWeight, blurMidWeight, blurMidWeight, blurMidWeight, blurLowWeight,
-	blurLowWeight, blurLowWeight, blurMidWeight, blurLowWeight, blurLowWeight}
+	{(1.0f / 40.0f), (1.0f / 40.0f), (2.0f / 40.0f), (1.0f / 40.0f), (1.0f / 40.0f),
+	(1.0f / 40.0f), (2.0f / 40.0f), (2.0f / 40.0f), (2.0f / 40.0f), (1.0f / 40.0f),
+	(2.0f / 40.0f), (2.0f / 40.0f), (4.0f / 40.0f), (2.0f / 40.0f), (2.0f / 40.0f),
+	(1.0f / 40.0f), (2.0f / 40.0f), (2.0f / 40.0f), (2.0f / 40.0f), (1.0f / 40.0f),
+	(1.0f / 40.0f), (1.0f / 40.0f), (2.0f / 40.0f), (1.0f / 40.0f), (1.0f / 40.0f)}
 };
 
 const uint32 normalKernel5x5Index = 0;
 
-const uint32 blurKernel5x5Index = 1;
+const uint32 sharpen1Kernel5x5Index = 1;
+
+const uint32 sharpen2Kernel5x5Index = 2;
+
+const uint32 outlineKernal5x5Index = 3;
+
+const uint32 embossKernel5x5Index = 4;
+
+const uint32 edge1Kernel5x5Index = 5;
+
+const uint32 edge2Kernel5x5Index = 6;
+
+const uint32 blurKernel5x5Index = 7;
 // ===== Image Kernels =====
